b7e39f3bc99b25a842da67979aa47d01cb7e0829
andre <driekie248@gmail.com>
2026-03-13 10:37:50 +0100

    header template made

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header(props) {
+    return(
+        <>
+        <div id="header">
+            <h1>HEader</h1>
+        </div>
+        </>
+    )
+
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './components/Header'
 
 function  App() {
     return(
         <>
+        <Header />
         <h1>Hello world</h1>
         </>
     )
