74052c708e5d61fbfe88c90221d90e5a08fb787e
andre <driekie248@gmail.com>
2026-03-13 09:51:38 +0100

    vite placeholder made

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+function  App() {
+    return(
+        <>
+        <h1>Hello world</h1>
+        </>
+    )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
