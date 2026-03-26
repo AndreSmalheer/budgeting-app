61b1e35c504c91229e0115a794851b1375ecc97b
andre <driekie248@gmail.com>
2026-03-26 09:56:16 +0100

    budget details pagina aangemaakt

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import StarterInhoud from "../pages/Starter-inhoud/Starter-inhoud";
 import HomePage from "../pages/HomePage/HomePage";
+import BudgetDetails from "../pages/BudgetDetails/BudgetDetails";
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/home-page" element={<HomePage />} />
       <Route path="/starter-inhoud" element={<StarterInhoud />} />
+      <Route path="/budget-details" element={<BudgetDetails/>} />
     </Routes>
   );
 }
---
+++ src/pages/BudgetDetails/BudgetDetails.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function BudgetDetails(){
+    return(
+        <>
+        <h1>Budgeting detials</h1>
+        </>
+    )
+}
+
+export default BudgetDetails
