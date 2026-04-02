de8e88f687790b7577a00008caa1dcfa41b0edeb
tim <102709@glr.nl>
2026-04-02 10:41:49 +0200

    werking tree

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App'
-import LandingPage from './pages/LandingPage/LandingPage';
-import StarterInhoud from './pages/Starter-inhoud/Starter-inhoud';
-import HomePage from './pages/HomePage/HomePage';
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home-page" element={<HomePage/>} />
-      <Route path="/starter-inhoud" element={<StarterInhoud />} />
-
-    </Routes>
-  );
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
